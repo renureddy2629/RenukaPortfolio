e72812c482b48806dad930a132c2125e95345ab5
Renuka Pasam <renukareddy442@gmail.com>
2024-02-09 22:45:04 -0600

    Changes in portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -17,25 +17,25 @@ const Portfolio = () => {
             title: 'XL Dashboard',
             description: 'A reporting application where instructors can view and understand student progress in given assignments.',
             technologies: ['React.js', 'JavaScript'], // Add any other relevant technologies]
-            image: './images/bg.jpeg'
+            image: './images/bg1.jpeg'
         },
         {
             title: 'Annual Enrollment',
             description: 'A process where employees of the Client are given a chance to enroll in Health Benefits for a new Plan Year.',
             technologies: ['Selenium WebDriver', 'Java'], // Add any other relevant technologies],
-            image: './images/bg.jpeg'
+            image: './images/bg3.jpg'
         },
         {
             title: 'FPL Ene-FPLES Support',
             description: 'Providing reliable and cost-effective solutions, combined with personalized service and exceptional customer care.',
-            technologies: ['C++'], // Add any other relevant technologies],
-            image: './images/bg.jpeg'
+            technologies: ['java','Selenium','Content authorized testing'], // Add any other relevant technologies],
+            image: './images/bg4.jpg'
         },
         {
             title: 'Text Classification using Machine Learning Techniques',
             description: 'A project that demonstrates the application of machine learning algorithms for text classification tasks.',
             technologies: ['Python', 'Machine Learning Libraries'],
-            image: './images/bg.jpeg'
+            image: './images/bg5.jpg'
         }
     ];
     console.log(projects)
